dc29860bd3dc32dfb05fcad26c69504b36c0480e
Jakob Diel <jakob.diel@hs-wismar.de>
2020-04-16 22:17:24 +0200

    pad funktion reduziert aber repariert. Anzahl Files für HSW korrigiert.

--- controllers/pad.php
+++ controllers/pad.php
@@ -20,20 +20,22 @@ class PadController extends ToolAssistantBaseController
     public function index_action()
     {
         $manager = \PluginManager::getInstance();
-        $padPlugin = $manager->getPlugin('EtherpadPlugin');
+        $padPlugin = $manager->getPlugin('StudIPadPlugin');
 
         if (!$manager->isPluginActivated($padPlugin->getPluginId(), $this->course_id)) {
             $manager->setPluginActivated($padPlugin->getPluginId(), $this->course_id, true);
         }
 
-        $padName = substr(preg_replace('/[^\w]/', '', \Context::get()->name), 0, 32);
+        $this->redirect(URLHelper::getURL('plugins.php/studipadplugin/show', array('cid' => $this->course_id)));
+
+      /*  $padName = substr(preg_replace('/[^\w]/', '', \Context::get()->name), 0, 32);
 
         if (!strlen($padName)) {
             throw new \RuntimeException(_("Es kann kein Pad-Name aus dem Veranstaltungsnamen generiert werden."));
         }
 
-        $url = \PluginEngine::getURL($padPlugin, ['new_pad_name' => $padName, 'if_not_exists' => 1], 'pads/create');
+        $url = \PluginEngine::getURL($padPlugin, ['new_pad_name' => $padName, 'if_not_exists' => 1], 'createPad');
 
-        $this->redirect($url);
+        $this->redirect($url);*/
     }
 }
